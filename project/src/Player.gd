036bbdba091266802beb7f2cfc186ab0d33651b5
Lucas Harris <laharris4@bsu.edu>
2021-10-22 10:58:17 -0400

    Implement Player movement

--- project/src/Player.gd
+++ project/src/Player.gd
@@ -3,8 +3,20 @@ extends KinematicBody2D
 
 
 export var player_health := 100
+export var player_movement_speed := 250
+
+var _ship_velocity := Vector2(0,0)
 
 
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
+func _physics_process(_delta):
+	_ship_velocity = Vector2(0,0)
+	if Input.is_action_pressed("move_up"):
+		_ship_velocity.y = player_movement_speed * -1
+	if Input.is_action_pressed("move_down"):
+		_ship_velocity.y = player_movement_speed
+	if Input.is_action_pressed("move_left"):
+		_ship_velocity.x = player_movement_speed * -1
+	if Input.is_action_pressed("move_right"):
+		_ship_velocity.x = player_movement_speed
+	
+	_ship_velocity = move_and_slide(_ship_velocity, Vector2.UP)
