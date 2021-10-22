c4755be072c4ed086326e0cd129a44751e4b4e3b
Lucas Harris <laharris4@bsu.edu>
2021-10-22 10:43:54 -0400

    Add Player ship sprite

---
+++ project/src/Player.gd
@@ -0,0 +1,10 @@
+class_name Player
+extends KinematicBody2D
+
+
+export var player_health := 100
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
