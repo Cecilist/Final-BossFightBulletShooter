ed03ba2d9b9c24131ab1ffcc72eb1d8f0bed23ee
Lucas Harris <laharris4@bsu.edu>
2021-10-22 13:21:31 -0400

    Add main menu and instructions scenes

---
+++ project/src/Instructions.gd
@@ -0,0 +1,11 @@
+class_name Instructions
+extends Node2D
+
+
+func _input(_event) -> void:
+	if Input.is_action_just_pressed("previous_menu"):
+		_on_BackButton_pressed()
+
+
+func _on_BackButton_pressed() -> void:
+	get_tree().change_scene("res://src/MainMenu.tscn")
---
+++ project/src/MainMenu.gd
@@ -0,0 +1,22 @@
+class_name MainMenu
+extends Node2D
+
+
+func _input(_event) -> void:
+	if Input.is_action_just_pressed("play_game"):
+		_on_PlayButton_pressed()
+	if Input.is_action_just_pressed("instructions"):
+		_on_InstructionsButton_pressed()
+	if Input.is_action_just_pressed("quit_game"):
+		_on_QuitButton_pressed()
+
+func _on_PlayButton_pressed() -> void:
+	get_tree().change_scene("res://src/Level.tscn")
+
+
+func _on_InstructionsButton_pressed() -> void:
+	get_tree().change_scene("res://src/Instructions.tscn")
+
+
+func _on_QuitButton_pressed() -> void:
+	get_tree().quit()
