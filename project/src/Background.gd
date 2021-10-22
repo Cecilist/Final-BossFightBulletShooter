99675bb4d4264d81a6facf93cfcff55bdaed3c0b
Lucas Harris <laharris4@bsu.edu>
2021-10-22 18:37:52 -0400

    Add looped scrolling background

---
+++ project/src/Background.gd
@@ -0,0 +1,13 @@
+extends ParallaxBackground
+
+
+# Starts scrolling the background
+func _ready():
+	$AnimationPlayer.play("Scroll")
+
+
+# Checks if the animation has finished, then resets the position
+#  of the second background layer
+func _on_AnimationPlayer_animation_finished(_anim_name) -> void:
+	$ParallaxLayer/SpaceBG2.position = Vector2(0, -2156.711)
+	$AnimationPlayer.play("Scroll")
