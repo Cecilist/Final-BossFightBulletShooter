7b1b71745b8ca4cfaebf7527acb8550bdc303b83
Lucas Harris <laharris4@bsu.edu>
2021-11-17 14:54:10 -0500

    Update background

--- project/Background/Background.gd
+++ project/Background/Background.gd
@@ -10,4 +10,6 @@ func _ready():
 #  of the second background layer
 func _on_AnimationPlayer_animation_finished(_anim_name):
 	$ParallaxLayer/SpaceBG2.position = Vector2(0, -2156.711)
+	$ParallaxLayer/SpaceBG4.position = Vector2(0, -2156.711)
+	$ParallaxLayer/SpaceBG6.position = Vector2(0, -2156.711)
 	$AnimationPlayer.play("Scroll")
