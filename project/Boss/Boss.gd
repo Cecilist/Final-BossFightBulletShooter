618c629a7cc71aa8296a57ca611334c03226ea41
Cecilist <Jcecil239@gmail.com>
2021-11-18 12:58:05 -0500

    added beginings of boss pattern switcher

--- project/Boss/Boss.gd
+++ project/Boss/Boss.gd
@@ -6,7 +6,6 @@ export var boss_health = 1000.0
 
 var boss_health_percent = 100.0
 var boss_paused = true
-var rng = RandomNumberGenerator.new()
 
 var _remaining_boss_health = boss_health
 var _boss_can_shoot = true
@@ -33,35 +32,14 @@ func _physics_process(_delta):
 
 # Creates instances of the bullets to fire at the player
 func _fire_bullet():
-	var bullet = load("res://Boss/BossBullet.tscn").instance()
-	rng.randomize()
-	var random_num =  floor(rng.randf_range(0, 5))
-	get_node("/root/Level").call_deferred("add_child", bullet)
-	if random_num == 0:
-		bullet.global_position = $Cannon1.global_position
-	if random_num == 1:
-		bullet.global_position = $Cannon2.global_position
-	if random_num == 2:
-		bullet.global_position = $Cannon3.global_position
-	if random_num == 3:
-		bullet.global_position = $Cannon4.global_position
-	if random_num == 4:
-		bullet.global_position = $Cannon5.global_position
+
 	
 	# Needs to be implemented in a less overwhelming way
 	#$BossShootingSound.play()
 	
-	# Code below will randomly move around cannons
-	#var randomX = rng.randf_range(-249, 269) # magic numbers representing current width of boss "sprite"
-	#var cannonMovement := Vector2(randomX, 97) # magic number represetning current general y location of cannons
-	#$Cannon1.set_position(cannonMovement)
-	#$Cannon2.set_position(cannonMovement)
-	#$Cannon3.set_position(cannonMovement)
-	#$Cannon4.set_position(cannonMovement)
-	#$Cannon5.set_position(cannonMovement)
-	
 	_boss_can_shoot = false
 	$BossShotTimer.start()
+	$PatternSwitcher.start()
 
 
 # Lowers the boss's health if they are hit
@@ -76,3 +54,11 @@ func _on_Area2D_area_entered(area):
 # Allows the boss to shoot again
 func _on_BossShotTimer_timeout():
 	_boss_can_shoot = true
+
+
+func _on_PatternSwitcher_timeout():
+	var spinner = load("res://Boss/Spinner/CannonSpinner.tscn").instance()
+	get_node("/root/Level/Boss").call_deferred("add_child", spinner)
+	spinner.global_position = $SpinnerSpawn1.position
+	spinner.global_position = $SpinnerSpawn2.position
+	
