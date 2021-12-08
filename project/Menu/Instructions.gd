fe7f66b9113320ec70aebabe53861abfccbbc880
Lucas Harris <laharris4@bsu.edu>
2021-12-08 18:24:16 -0500

    Add title screen transitions

--- project/Menu/Instructions.gd
+++ project/Menu/Instructions.gd
@@ -2,6 +2,10 @@ class_name Instructions
 extends Node2D
 
 
+func _ready():
+	$Transition.fade_in()
+
+
 # Checks if the player pressed the key to return to the main menu
 func _input(_event):
 	if Input.is_action_just_pressed("previous_menu"):
@@ -9,5 +13,12 @@ func _input(_event):
 
 
 # Returns to the menu
+func _process(_delta):
+	if $Transition.can_switch_scene == true:
+		var _ignored := get_tree().change_scene("res://Menu/MainMenu.tscn")
+
+
+# Returns to the menu
 func _on_BackButton_pressed():
-	var _ignored := get_tree().change_scene("res://Menu/MainMenu.tscn")
+	$Transition.show_rect()
+	$Transition.fade_out()
